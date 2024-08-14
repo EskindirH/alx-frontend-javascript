c80cd4f5c11ba052c2e315b9cbac652086ab92c4
EskindirH <eskindirhaileliul@gmail.com>
2024-08-14 14:01:48 +0000

    done

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,45 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: "Sara",
+	lastName: "Abebe",
+	age: 23,
+	location: "Addis"
+}
+
+const student2: Student = {
+	firstName: "Eskindir",
+	lastName: "Birhanu",
+	age: 21,
+	location: "Addis"
+}
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+studentsList.forEach((student: Student): void => {
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('td');
+  const locationCell = document.createElement('td');
+
+  nameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  nameCell.style.border = "1px solid pink";
+  locationCell.style.border = "1px solid pink";
+  nameCell.style.padding = "5px";
+  locationCell.style.padding = "5px";
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+  tbody.appendChild(row);
+});
+
+document.body.appendChild(table);
